acddfaaf5f043e1684d8a4cc4d0defaffa9b5097
yakir <notcooldudet@gmail.com>
2022-04-28 16:27:01 +0300

    hi

---
+++ bool IsHelthy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bool_IsHelthy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int cal;
+            int fat;
+            int count = 0;
+            int maxCal = 0;
+            string nameOfmax = "";
+            Console.WriteLine("please enter the name of your product");
+            name = Console.ReadLine();
+            while (name != "Stop") 
+            {
+                Console.WriteLine("please enter calories");
+                cal = int.Parse(Console.ReadLine());
+                Console.WriteLine("please enter fat");
+                fat = int.Parse(Console.ReadLine());
+                Box c = new Box(name, cal, fat);
+                if (c.IsHelthy())
+                      count++;
+                if( c.GetCal () > maxCal)
+                {
+                    maxCal = c.GetCal();
+                    nameOfmax = c.GetCal();
+                }
+                Console.WriteLine("please enter name of produck");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine("Number of produck that not bad for health " + count);
+            Console.WriteLine(nameOfmax + " has maximum calories");
+        }
+    }
+}
---
+++ bool IsHelthy/class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bool_IsHelthy
+{
+    class Box
+    {
+        int fat;
+         int cal;
+        private string name;
+
+        public Box(string name, int cal, int fat)
+        {
+            this.name = name;
+            this.cal = cal;
+            this.fat = fat;
+        }
+
+        public bool IsHelthy()
+        {
+            return this.cal < 130 && this.fat < 5;
+        }
+    }
+}
